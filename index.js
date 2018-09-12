22ae597168b40f446148c386c0e57a2901755d3e
Manny Shapir <mshapir95@gmail.com>
2018-09-12 18:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var name = "Susan"
-if (name === "Susan") { 
-  console.log("The name is correct")
-} else {
-  console.log("Excpected the name to be Susan")
-}
+var name = "Joe"
 var height = "74"
 
 
